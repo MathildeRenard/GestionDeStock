521d8df3d7285854921cdfaf67f2e156ffc3bdfe
Nicolas <v.nicolasvanhuysse@free.fr>
2021-10-29 12:01:54 +0200

    Creation classe db

---
+++ STIVE_GestionStock/Models/Db.cs
@@ -0,0 +1,20 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace STIVE_GestionStock.Models
+{
+    public class Db
+    {
+        public Db()
+        {
+        }
+
+        private static string connectionString = "Server=127.0.0.1;DataBase=STIVE;UserId=root;password=";
+        public static MySqlConnection Connection { get => new MySqlConnection(connectionString); }
+
+    }
+
+}
