1b5b408b5bf76ad1a1cf2ec0a4235de8d0f31b27
Mathilde Renard <mathilderenard626@gmail.com>
2022-02-17 15:21:31 +0100

    userController

---
+++ STIVE_GestionStock/ViewModels/UserViewModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using STIVE_GestionStock.Models;
+
+namespace STIVE_GestionStock.ViewModels
+{
+    public class UserViewModels
+    {
+        //Cette classe permettra de passer deux paramètres à la vue update de employee
+
+
+        public List<Role> listRoles { get; set; }
+        public User user { get; set; }
+
+        }
+    }
+
+
